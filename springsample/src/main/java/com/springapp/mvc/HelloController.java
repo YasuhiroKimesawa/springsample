bc2bf96ed497d22b0bc862c855881cf364d4a924
kimesawa <pilgrim.ericclapton@gmail.com>
2014-03-10 04:32:59 +0900

    簡単なサンプル

--- springsample/src/main/java/com/springapp/mvc/HelloController.java
+++ springsample/src/main/java/com/springapp/mvc/HelloController.java
@@ -1,16 +1,29 @@
 package com.springapp.mvc;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 @RequestMapping("/")
-public class HelloController {
-	@RequestMapping(method = RequestMethod.GET)
-	public String printWelcome(ModelMap model) {
-		model.addAttribute("message", "Hello world!");
-		return "hello";
-	}
+public class HelloController
+{
+
+
+    @RequestMapping(method = RequestMethod.GET)
+    public String newName( Model model )
+    {
+        User user = new User();
+        model.addAttribute( "user", user );
+        return "newname";
+    }
+
+    @RequestMapping(method = RequestMethod.POST)
+    public String idCheck( Model model, User user )
+    {
+        model.addAttribute( "user", user );
+        return "newname";
+    }
 }
---
+++ springsample/src/main/java/com/springapp/mvc/User.java
@@ -0,0 +1,30 @@
+package com.springapp.mvc;
+
+import java.io.Serializable;
+
+public class User implements Serializable
+{
+    private String name;
+
+    public User( String name )
+    {
+        this.name = name;
+    }
+
+    public String getName()
+    {
+        return name;
+    }
+
+    public void setName( String name )
+    {
+        this.name = name;
+    }
+
+    public User()
+    {
+        this.name = "";
+    }
+
+    private static final long serialVersionUID = -7154353760999600451L;
+}
